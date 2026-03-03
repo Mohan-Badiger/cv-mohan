97dffb623162142a1aac22e6a8f90bdde6591fd4
Mohan-Badiger <mohanbadiger250@gmail.com>
2026-03-03 17:38:31 +0530

    updated App and Home pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ const App = () => {
   return (
     <div className="min-h-screen bg-white dark:bg-black text-black dark:text-white">
        <main className="pt-20 mx-auto w-full max-w-5xl px-4 sm:px-6 lg:px-8">
-        {/* Sections will go here */}
       <Home/>
       </main>
     </div>
